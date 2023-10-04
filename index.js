eeccdc10820454229b12cefe65dcb3ada5ff070d
Shashank Naik <shashanknaik808@gmail.com>
2023-10-04 10:56:49 +0530

    Built Connection b/w NodeJs & MySQL

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const mysql = require('mysql');
 const express = require('express');
+
 const connection = require('./model/connection.js');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+connection.connect(err => {
+    if (err) console.log("There is an error while connecting" + err);
+
+    app.listen(4000);
+
+    console.log("DB Connected" + connection.threadId);
+    console.log("Server started at port 4000");
+});
